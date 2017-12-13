febdec6dd3846c1e6c743ea4e50da79485018b35
Alexis <alexis.saransig@jobsity.com>
2017-12-13 01:30:26 -0500

    Updating codebase to select data from mongo

--- node2micro/1-monolith/server.js
+++ node2micro/1-monolith/server.js
@@ -1,16 +1,11 @@
 // Load required libraries.
 const app = require('koa')();
 const router = require('koa-router')();
-var mongo = require('mongodb').MongoClient;
 var mysql = require('mysql');
 
-// Load a file with Sample texts.
-const dd = require('./dumbdata.json');
-// Set the limit of data to be generated.
-const limit = 200000;
+require("./mongoApp")(app);
 
 // Set connection string/info.
-var conn_mongo = "mongodb://localhost:27017/node2micro";
 var conn_mysql = mysql.createConnection({
   host     : '127.0.0.1',
   port     : '3306',
@@ -32,13 +27,13 @@ app.use(function *(next){
 
 // Generate Users - Mongo
 router.get('/api/generate/users', function *(next) {
-  yield generateUsers;
+  yield app.MgenerateUsers;
   this.body = "Users - Ok";
 });
 
 // Generate Threads - Mongo
 router.get('/api/generate/threads', function *(next) {
-  yield generateThreads;
+  yield app.generateThreads;
   this.body = "Threads - Ok";
 });
 
@@ -50,21 +45,21 @@ router.get('/api/generate/posts', function *(next) {
 
 // Extra endpoints to retrieve existing data.
 router.get('/api/users', function *(next) {
-  this.body = db.users;
+  this.body = yield app.users.find().limit(100).toArray();
 });
 
 router.get('/api/users/:userId', function *(next) {
   const id = parseInt(this.params.userId);
-  this.body = db.users.find((user) => user.id == id);
+  this.body = yield app.users.find({_id: id}).toArray();
 });
 
 router.get('/api/threads', function *() {
-  this.body = db.threads;
+  this.body = yield app.threads.find().limit(100).toArray();
 });
 
 router.get('/api/threads/:threadId', function *() {
   const id = parseInt(this.params.threadId);
-  this.body = db.threads.find((thread) => thread.id == id);
+  this.body = yield app.threads.find({_id: id}).toArray();
 });
 
 router.get('/api/posts/in-thread/:threadId', function *() {
@@ -84,88 +79,6 @@ router.get('/api/', function *() {
 router.get('/', function *() {
   this.body = "Ready to receive requests";
 });
-
-
-// ------------------- HELPER FUNCTIONS --------------------
-
-// Function to generate Users data.
-function generateUsers(callback) {
-  mongo.connect(conn_mongo, function(e1, db) {
-    if (e1) throw e1;
-    
-    var dbase = db.db("mydb");
-
-    // Drop the existing collection to avoid duplicated ids.
-    dbase.collection('users').drop();
-
-    // Generate the collection.
-    dbase.createCollection("users", function(e2, coll) {
-
-      var obj_users = [];
-
-      // Generate ramdon values for users data.
-      for (var i = 0; i < limit; i++) {    
-        var a = Math.floor((Math.random() * 9) + 1);
-        var b = Math.floor((Math.random() * 9) + 1);
-        var uname = dd.names[a];
-        var ubio = dd.text[b];
-        var obj_user = {
-          _id: i,
-          username: 'user-' + uname.toLowerCase() + '-' + i,
-          name: uname + '-' + i,
-          bio: ubio
-        }
-
-        obj_users.push(obj_user);
-      }
-
-      // Insert in the collection the values.
-      coll.insertMany(obj_users, function(err, result) {
-        console.log(limit + " Users generated!");
-        db.close();
-        callback();
-      });
-    });
-  });
-}
-
-// Function to generate Users data.
-function generateThreads(callback) {
-  mongo.connect(conn_mongo, function(e1, db) {
-    if (e1) throw e1;
-    
-    var dbase = db.db("mydb");
-
-    // Drop the existing collection to avoid duplicated ids.
-    dbase.collection('threads').drop();
-
-    // Generate the collection.
-    dbase.createCollection("threads", function(e2, coll) {
-
-      var obj_threads = [];
-
-      for (var i = 0; i < limit; i++) {
-        var a = Math.floor((Math.random() * 9) + 1);
-        var uid = Math.floor((Math.random() * (limit - 1)) + 1);
-        var title = dd.titles[a];
-        var obj_thread = {
-          _id: i,
-          title: title + '-' + i, 
-          author: uid
-        };
-
-        obj_threads.push(obj_thread);
-      }
-
-      // Insert in the collection the values.
-      coll.insertMany(obj_threads, function(err, result) {
-        console.log(limit + " Threads generated!");
-        db.close();
-        callback();
-      });
-    });
-  });
-}
 
 // Function to generate Posts data.
 function generatePosts(callback) {
